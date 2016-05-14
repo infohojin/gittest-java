e3de5944e9e10da2c218e8d02bb26cb1cf5f81c3
hojine <infohojin@gmail.com>
2016-05-14 16:56:08 +0900

    bugfix

--- lottogenerator.java
+++ lottogenerator.java
@@ -6,7 +6,7 @@ public class LottoGenerator {
 	public int[] generate() { 
 		int[] result = new int[6]; 
 			for(int i = 0; i < 6; i++) { 
-				result[i] = random.nextInt(45); 
+				result[i] = random.nextInt(45) + 1; 
 			} 
 
 			return result; 
